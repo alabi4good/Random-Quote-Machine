6ad10eb01e928d70ce83cbf4de0d06ad57d5c2fd
alabi4code <adetunjialabi@yahoo.com>
2017-10-16 13:15:56 -0500

    fixed a bug in the js file

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ $(function(){
        var color = ['#3498db', '#2ecc71', '#1abc9c', '#16a085', '#f1c40f', '#c0392b'];
        
         $.ajax({
-            url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+            url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
             success: (data)=> {
                 var post = data.shift();
                 var randomNum = Math.floor(Math.random() * 6);
